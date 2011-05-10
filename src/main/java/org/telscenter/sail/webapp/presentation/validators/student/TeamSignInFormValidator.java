bb903f050800086506e19d44263e4c3f3f9185fc
Geoffrey Kwan <geoffreykwan@gmail.com>
2011-05-10 22:47:50 +0000

    Disallowed entering a teacher username as a team mate when signing into a run. Also check that team mate passwords are correct now.

--- src/main/java/org/telscenter/sail/webapp/presentation/validators/student/TeamSignInFormValidator.java
+++ src/main/java/org/telscenter/sail/webapp/presentation/validators/student/TeamSignInFormValidator.java
@@ -23,12 +23,17 @@
 package org.telscenter.sail.webapp.presentation.validators.student;
 
 import net.sf.sail.webapp.domain.User;
+import net.sf.sail.webapp.domain.authentication.MutableUserDetails;
 import net.sf.sail.webapp.service.UserService;
 
 import org.apache.commons.lang.StringUtils;
+import org.springframework.security.providers.dao.salt.SystemWideSaltSource;
+import org.springframework.security.providers.encoding.Md5PasswordEncoder;
+import org.springframework.security.providers.encoding.PasswordEncoder;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
+import org.telscenter.sail.webapp.domain.authentication.impl.TeacherUserDetails;
 import org.telscenter.sail.webapp.presentation.web.TeamSignInForm;
 
 /**
@@ -40,6 +45,7 @@ import org.telscenter.sail.webapp.presentation.web.TeamSignInForm;
 public class TeamSignInFormValidator implements Validator {
 
 	private UserService userService;
+	private SystemWideSaltSource systemSaltSource;
 	
 	/**
 	 * @see org.springframework.validation.Validator#supports(java.lang.Class)
@@ -54,6 +60,7 @@ public class TeamSignInFormValidator implements Validator {
 	 */
 	public void validate(Object teamSignInFormIn, Errors errors) {
 		TeamSignInForm teamSignInForm = (TeamSignInForm) teamSignInFormIn;
+		PasswordEncoder encoder = new Md5PasswordEncoder();
 		
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username1", 
 				"error.teamsignin-username-not-specified");
@@ -72,27 +79,51 @@ public class TeamSignInFormValidator implements Validator {
 		// but not user2
 		if (!StringUtils.isEmpty(teamSignInForm.getUsername2())) {
 			if (StringUtils.isEmpty(teamSignInForm.getPassword2())) {
+				//the password field is empty
 				errors.rejectValue("password2", "error.teamsignin-password-not-specified");
 			} else {
 				User user2 = userService.retrieveUserByUsername(teamSignInForm.getUsername2());
 				if (user2 == null) {
+					//the username does not exist
 					errors.rejectValue("username2", "error.teamsignin-user-does-not-exist");
-				} else if (user2.getUserDetails().getPassword() != teamSignInForm.getPassword2()) {
-					//TODO FIGURE OUT HOW TO COMPARE PASSWORDS
-					//errors.rejectValue("password2", "error.teamsignin-incorrect-password");
+				} else {
+					//get the user and the hashed password
+					MutableUserDetails userDetails2 = user2.getUserDetails();
+					String hashedPassword2 = encoder.encodePassword(teamSignInForm.getPassword2(), systemSaltSource.getSystemWideSalt());
+
+					if(userDetails2 instanceof TeacherUserDetails) {
+						//username is for a teacher and that is not allowed
+						errors.rejectValue("username2", "error.teamsignin-teacher-username-specified");
+					} else if (!user2.getUserDetails().getPassword().equals(hashedPassword2)) {
+						//password is incorrect
+						errors.rejectValue("password2", "error.teamsignin-incorrect-password");
+					}
 				}
 			}
 		}
 		
+		//handle user3
 		if (!StringUtils.isEmpty(teamSignInForm.getUsername3())) {
 			if (StringUtils.isEmpty(teamSignInForm.getPassword3())) {
+				//the password field is empty
 				errors.rejectValue("password3", "error.teamsignin-password-not-specified");
 			} else {
 				User user3 = userService.retrieveUserByUsername(teamSignInForm.getUsername3());
 				if (user3 == null) {
+					//the username does not exist
 					errors.rejectValue("username3", "error.teamsignin-user-does-not-exist");
-				} else if (user3.getUserDetails().getPassword() != teamSignInForm.getPassword3()) {
-					//errors.rejectValue("password3", "error.teamsignin-incorrect-password");
+				} else {
+					//get the user and the hashed password
+					MutableUserDetails userDetails3 = user3.getUserDetails();
+					String hashedPassword3 = encoder.encodePassword(teamSignInForm.getPassword3(), systemSaltSource.getSystemWideSalt());
+					
+					if(userDetails3 instanceof TeacherUserDetails) {
+						//username is for a teacher and that is not allowed
+						errors.rejectValue("username3", "error.teamsignin-teacher-username-specified");
+					} else if (!user3.getUserDetails().getPassword().equals(hashedPassword3)) {
+						//password is incorrect
+						errors.rejectValue("password3", "error.teamsignin-incorrect-password");
+					}					
 				}
 			}
 		}
@@ -112,4 +143,19 @@ public class TeamSignInFormValidator implements Validator {
 		this.userService = userService;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public SystemWideSaltSource getSystemSaltSource() {
+		return systemSaltSource;
+	}
+
+	/**
+	 * 
+	 * @param systemSaltSource
+	 */
+	public void setSystemSaltSource(SystemWideSaltSource systemSaltSource) {
+		this.systemSaltSource = systemSaltSource;
+	}
 }
