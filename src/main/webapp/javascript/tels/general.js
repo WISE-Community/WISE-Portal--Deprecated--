662e5f218ffbd8a8357e7e2531445ba8551516a1
breity <breity10@gmail.com>
2012-05-18 14:02:15 -0700

    Add miniTip tooltip library and insertTooltips utility function which automatically adds tooltips to selected DOM elements on a page

--- src/main/webapp/javascript/tels/general.js
+++ src/main/webapp/javascript/tels/general.js
@@ -1,52 +1,93 @@
+//TODO: Convert to prototype format
 
-var wiseImages = new Array('./themes/tels/default/images/wiseInAction/kidsOnComputer.jpg','./themes/tels/default/images/wiseInAction/kidsinaquarium.jpg','./themes/tels/default/images/wiseInAction/csiScreenshot.jpg','./themes/tels/default/images/wiseInAction/Studentputdata.jpg','./themes/tels/default/images/wiseInAction/AirBag.jpg','./themes/tels/default/images/wiseInAction/Reflection.jpg','./themes/tels/default/images/wiseInAction/PondSim.jpg','./themes/tels/default/images/wiseInAction/OnlineDiscussion.jpg',
-'./themes/tels/default/images/wiseInAction/MolecularModel.jpg','./themes/tels/default/images/wiseInAction/SkinCells.jpg');
-
-function swapImage(id,link){
-	if(document.images){
-		document.getElementById(id).src=link;
-	}
-};
-
-function swapWelcomeMsg(index) {
-	$('.welcomeBullet').hide();
-	$('.welcomeLink').removeClass('active');
-	$('.welcomeLink:eq('+ index +')').addClass('active');
-	$('.welcomeBullet:eq('+ index +')').fadeIn('slow',function(){
-		$(this).removeAttr('filter');
+/*
+ * Finds any DOM elements with the 'tooltip' class and initializes the miniTip plugin on each.
+ * 
+ * @param options An object to specify default miniTip settings for all tooltips (Optional; 
+ * see http://goldfirestudios.com/blog/81/miniTip-jQuery-Plugin for allowable options)
+ * 
+ * Individual tooltip options can be customized by adding additional attributes to the target DOM 
+ * element (optional):
+ * - tooltip-event:'click' sets the tooltip to render on mouse click (vs. hover, which is the default)
+ * - tooltip-anchor:'bottom', 'left', and 'right' set the positions of the tooltip to bottom, left, 
+ * and right respectively (default is top)
+ * - tooltip-maxW:'XXXpx' sets the max-width of the tooltip element to XXX pixels (default is '250px');
+ * - tooltip-content: String (or HTML String) to set as the tooltip's content (default is the element's 
+ * title attribute)
+ * - tooltip-title: String to set as the tooltip's title (default is no title)
+ * - tooltip-class: String to add to the tooltip element's css class (default is none)
+ */
+insertTooltips = function(options){
+	// for all DOM elements with the 'tooltip' class, initialize miniTip
+	$('.tooltip').each(function(){
+		// set miniTip default options
+		var settings = {};
+		if(options != null && typeof options == 'object'){
+			// options have been sent in as a parameter
+			settings = options;
+		} else {
+			// options have not been sent in as a paremeter, so set them 
+			settings = {
+				anchor:'n',
+				event:'hover',
+				aHide:false,
+				maxW:'250px',
+				fadeIn:1,
+				fadOut:1,
+				delay:100,
+				show: function(){},
+				hide: function(){}
+			};
+		}
+		
+		// set options based on target element attributes
+		if($(this).attr('tooltip-event') == 'click'){
+			settings.event = 'click';
+		}
+		if($(this).attr('tooltip-anchor') == 'right'){
+			settings.anchor = 'e';
+		} else if ($(this).attr('tooltip-anchor') == 'bottom'){
+			settings.anchor = 's';
+		} else if ($(this).attr('tooltip-anchor') == 'left'){
+			settings.anchor = 'w';
+		}
+		if($(this).attr('tooltip-maxW') && $(this).attr('tooltip-maxW').match(/^[0-9]+px$/)){
+			settings.maxW = $(this).attr('tooltip-maxW');
+		}
+		if(typeof $(this).attr('tooltip-content') == 'string'){
+			settings['content'] = $(this).attr('tooltip-content');
+		}
+		if(typeof $(this).attr('tooltip-title') == 'string'){
+			settings['title'] = $(this).attr('tooltip-title');
+		}
+		if(typeof $(this).attr('tooltip-class') == 'string'){
+			var doShow = settings.show, doHide = settings.hide;
+			settings.show = function(){
+				$('#miniTip').addClass($(this).attr('tooltip-class'));
+				doShow();
+			};
+			settings.hide = function(){
+				setTimeout(function(){$('#miniTip').removeClass($(this).attr('tooltip-class'));},200);
+				doHide();
+			};
+		} else {
+			var doShow = settings.show;
+			settings.show = function(){
+				$('#miniTip').attr('class','');
+				doShow();
+			};
+		}
+		
+		// initialize miniTip on element
+		$(this).miniTip(settings);
+		
+		// remove all tooltip attributes and class from DOM element (to clean up html and so item are not re-processed if insertTooltips is called again on same page)
+		$(this).removeAttr('tooltip-event').removeAttr('tooltip-anchor').removeAttr('tooltip-maxW').removeAttr('tooltip-content').removeAttr('tooltip-title').removeClass('tooltip');
 	});
 };
 
-function changeNavigationColor(id,link){
-	if(document.images){
-		document.getElementById(id).src = link;
-	}
-}
 
-function displayNotAvailable(message){
-	alert(message);	
-}
-
-function moveToImage(id,value){
-	if((value >=0) && (value < 10)){
-		document.getElementById(id).src = wiseImages[value-1];	
-	}
-}
-
-function changeText(id,value){
-  value = value % 10;
-  value += 1;
-  document.getElementById(id).innerHTML= value + ' of 10';	
-}
-
-function changeText_T(id,value){
-  value = value % 5;
-  value += 1;
-  document.getElementById(id).innerHTML= value + ' of 5';	
-}
-
-
-/*These two functions are used for the FORM HINT functionality on registration pages */
+/*These two functions are used for the FORM HINT functionality on registration pages*/
 
 function addLoadEvent(func) {
   var oldonload = window.onload;
@@ -97,68 +138,3 @@ function prepareInputsForHints() {
   }
 }
 addLoadEvent(prepareInputsForHints);
-
-
-// Added my MattFish to handle Rollover behavior on Home Page (index.html) //
-
-/*function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
-
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
-}
-
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}*/
-
-// Added my MattFish to handle special Pop-Up Windows on Teacher Dashboard pages //
-
-function popupSpecial(mylink, windowname)
-		{
-		if (! window.focus)return true;
-		var href;
-		if (typeof(mylink) == 'string')
-		   href=mylink;
-		else
-		   href=mylink.href;
-		window.open(href, windowname, 'width=850,height=600,resizable=yes,scrollbars=yes');
-		return false;
-		}
-		
-
-function popup640(mylink, windowname)
-		{
-		if (! window.focus)return true;
-		var href;
-		if (typeof(mylink) == 'string')
-		   href=mylink;
-		else
-		   href=mylink.href;
-		window.open(href, windowname, 'width=640,height=480,resizable=yes,scrollbars=yes');
-		return false;
-		}
-		
-function popup300(mylink, windowname)
-		{
-		if (! window.focus)return true;
-		var href;
-		if (typeof(mylink) == 'string')
-		   href=mylink;
-		else
-		   href=mylink.href;
-		window.open(href, windowname, 'width=300,height=300,resizable=yes,scrollbars=yes');
-		return false;
-		}
-		
